69a0a0515d72a3ff205a44986dc37f4b361761b2
Firat Yildirim <fy@smartlockers.io>
2021-10-11 14:06:24 +0200

    Game

---
+++ JeuxConsole/Personnage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuxConsole
+{
+    class Personnage
+    {
+        public string Nom { get; set; }
+        public string Type { get; set; }
+        public int PV { get; set; }
+        public int Force { get; set; }
+        public int Resistance { get; set; }
+        public int Vitesse { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public string Position { get { return Nom + "(" + PositionX + ";" + PositionY + ")"; } }
+        public string Information { get { return Nom + "(" + Type + ")" + Environment.NewLine + "PV:" + PV + " Force:" + Force + " Résistance:" + Resistance + " Vitesse:" + Vitesse; } }
+
+        public Personnage(string vNom, string vType, int vPV, int vForce, int vResistance, int vVitesse, int vPositionX, int VPositionY)
+        {
+            Nom = vNom;
+            Type = vType;
+            PV = vPV;
+            Force = vForce;
+            Resistance = vResistance;
+            Vitesse = vVitesse;
+            PositionX = vPositionX;
+            PositionY = VPositionY;
+        }
+
+        public int Attaque(Personnage ennemi)
+        {
+            int degats = Force - ennemi.Resistance;
+            if (degats < 0)
+            {
+                degats = 1;
+            }
+            ennemi.PV -= degats;
+            if (ennemi.PV < 0)
+            {
+                ennemi.PV = 0;
+            }
+            Console.WriteLine(Nom + " attaque ! --> " + ennemi.Nom + " " + ennemi.PV + "PV");
+            return degats;
+        }
+
+        public static void Combat(Personnage attaquant, Personnage defenseur)
+        {
+            Console.Clear();
+
+            Console.WriteLine(attaquant.Information);
+            Console.WriteLine("VS");
+            Console.WriteLine(defenseur.Information);
+
+            Personnage p1, p2;
+            if (attaquant.Vitesse >= defenseur.Vitesse)
+            {
+                p1 = attaquant;
+                p2 = defenseur;
+            }
+            else
+            {
+                p1 = defenseur;
+                p2 = attaquant;
+            }
+
+            while (p1.PV > 0 && p2.PV > 0)
+            {
+                p1.Attaque(p2);
+                if (p2.PV > 0)
+                {
+                    p2.Attaque(p1);
+                }
+            }
+
+            if (p1.PV <= 0)
+            {
+                Console.WriteLine(p1.Nom + " a été vaincu !");
+            }
+            else
+            {
+                Console.WriteLine(p2.Nom + " a été vaincu !");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JeuxConsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuxConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carte stade =  new Carte(20, 70);
+            stade.Ennemis.Add(new Personnage("Gobelin", "Monstre", 5, 2, 1, 2, 15, 18));
+            stade.Ennemis.Add(new Personnage("Gobelin", "Monstre", 5, 2, 1, 2, 19, 66));
+            stade.Ennemis.Add(new Personnage("Gobelin", "Monstre", 5, 2, 1, 2, 3, 44));
+            stade.Ennemis.Add(new Personnage("Gobelin", "Monstre", 5, 2, 1, 2, 6, 55));
+            stade.Ennemis.Add(new Personnage("Gobelin", "Monstre", 5, 2, 1, 2, 12, 24));
+
+            Console.Clear();
+
+            Console.WriteLine("Bienvenue aventurier, quel est ton nom ?");
+            string Nom = Console.ReadLine();
+
+            Console.WriteLine("Quelles sont vos compétences ? 1=Guerrier 2=Ranger 3=Voleur");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    stade.Heros = new Personnage(Nom, "Guerrier", 150, 10, 10, 1, 5, 5);
+                    break;
+                case "2":
+                    stade.Heros = new Personnage(Nom, "Ranger", 100, 7, 7, 5, 5, 5);
+                    break;
+                case "3":
+                    stade.Heros = new Personnage(Nom, "Voleur", 75, 10, 5, 10, 5, 5);
+                    break;
+                default:
+                    stade.Heros = new Personnage(Nom, "Vagabond", 25, 1, 1, 1, 5, 5);
+                    break;
+            }
+            while (stade.Heros.PV > 0)
+            {
+                stade.AfficherCarte();
+
+                stade.Deplacement();
+
+                stade.Contact();
+            }
+            Console.WriteLine("GAME OVER");
+            Console.ReadLine();
+        }
+    }
+}
